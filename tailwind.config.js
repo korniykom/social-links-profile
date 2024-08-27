e5fad6e7c0226262b3eff2edd0c8d634205d4a3c
Maksym Korniiko <korniykom@gmail.com>
2024-08-27 22:45:12 +0300

    fix: change font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily: "Inter",
+      fontFamily: { sans: "Inter" },
       transitionTimingFunction: {
         custom: "cubic-bezier( 0, 1, 0.3, 1 )",
       },
