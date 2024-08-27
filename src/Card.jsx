d3ab78e47d98cd3a660d2ee6ce532e5d7997c550
Maksym Korniiko <korniykom@gmail.com>
2024-08-27 21:05:24 +0300

    fix: change location from h2 to p

--- src/Card.jsx
+++ src/Card.jsx
@@ -7,7 +7,7 @@ export default function Card() {
       <img src={image} alt="Jessica" className="m-6 w-[88px] rounded-full" />
       <div className="text-center font-semibold">
         <h1 className="mb-1 text-2xl text-white">Jessica Randall</h1>
-        <h2 className="text-sm text-[#c4f82a]">London, United Kingdom</h2>
+        <p className="text-sm text-[#c4f82a]">London, United Kingdom</p>
       </div>
       <q className="m-6 text-sm font-normal text-white">
         Front-end developer and avid reader
