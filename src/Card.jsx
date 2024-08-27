4f4e356e595d97f4e1615d1f94ebf16b060fb8d3
Maksym Korniiko <korniykom@gmail.com>
2024-08-27 22:35:02 +0300

    fix: image margin

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,8 +3,8 @@ import image from "./assets/images/avatar-jessica.jpeg";
 
 export default function Card() {
   return (
-    <div className="m-6 flex max-h-screen max-w-[20.5rem] grow flex-col items-center overflow-y-auto rounded-xl bg-[#1f1f1f] p-6 md:max-w-[28.5rem] xl:max-w-[24rem]">
-      <img src={image} alt="Jessica" className="m-6 w-[5.5rem] rounded-full" />
+    <div className="mb-6 flex max-h-screen max-w-[20.5rem] grow flex-col items-center overflow-y-auto rounded-xl bg-[#1f1f1f] p-6 md:max-w-[28.5rem] xl:max-w-[24rem]">
+      <img src={image} alt="Jessica" className="mb-6 w-[5.5rem] rounded-full" />
       <div className="text-center font-semibold">
         <h1 className="mb-1 text-2xl text-white">Jessica Randall</h1>
         <p className="text-sm text-[#c4f82a]">London, United Kingdom</p>
