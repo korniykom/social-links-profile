1c6ed645f6a9cc0119d6420322c03b552ccd180b
Maksym Korniiko <korniykom@gmail.com>
2024-08-27 22:00:08 +0300

    fix: change max-width from px to rem

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,8 +3,8 @@ import image from "./assets/images/avatar-jessica.jpeg";
 
 export default function Card() {
   return (
-    <div className="m-6 flex max-h-screen max-w-[327px] flex-col items-center overflow-y-auto rounded-xl bg-[#1f1f1f] p-6">
-      <img src={image} alt="Jessica" className="m-6 w-[88px] rounded-full" />
+    <div className="m-6 flex max-h-screen max-w-[2-rem] flex-col items-center overflow-y-auto rounded-xl bg-[#1f1f1f] p-6">
+      <img src={image} alt="Jessica" className="m-6 w-[5.5rem] rounded-full" />
       <div className="text-center font-semibold">
         <h1 className="mb-1 text-2xl text-white">Jessica Randall</h1>
         <p className="text-sm text-[#c4f82a]">London, United Kingdom</p>
--- src/Link.jsx
+++ src/Link.jsx
@@ -2,7 +2,7 @@ export default function Link({ title = "No title", URL = "#" }) {
   return (
     <li className="w-full">
       <a
-        className="flex h-[45px] w-full items-center justify-center rounded-lg bg-[#333] text-sm font-bold text-white transition duration-[3000ms] ease-custom hover:bg-[#c4f82a] hover:text-[#333]"
+        className="flex h-[2.8125rem] w-full items-center justify-center rounded-lg bg-[#333] text-sm font-bold text-white transition duration-[3000ms] ease-custom hover:bg-[#c4f82a] hover:text-[#333]"
         href={URL}
       >
         {title}
