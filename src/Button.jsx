c6db61a9bb638d78d33bdcc4c2e3e2b8b9f5ab68
Maksym Korniiko <korniykom@gmail.com>
2024-08-27 18:06:30 +0300

    feat: add custom animation & increase animation to 3 seconds

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button({ title = "No title" }) {
   return (
-    <div className="hover:animate-fade-in flex h-[45px] w-[279px] items-center justify-center rounded-lg bg-[#333] text-sm font-bold text-white transition-all duration-700 hover:bg-[#c4f82a] hover:text-[#333]">
+    <div className="ease-custom flex h-[45px] w-[279px] items-center justify-center rounded-lg bg-[#333] text-sm font-bold text-white transition duration-[3000ms] hover:bg-[#c4f82a] hover:text-[#333]">
       {title}
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,26 +5,7 @@ export default {
     extend: {
       fontFamily: "Inter",
       transitionTimingFunction: {
-        "in-expo": "cubic-bezier(0.95, 0.05, 0.795, 0.035)",
-        "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)",
-      },
-      keyframes: {
-        "fade-out": {
-          "0%": {
-            opacity: 1,
-          },
-          "100%": {
-            opacity: 0,
-          },
-        },
-      },
-      "fade-in": {
-        "0%": {
-          opacity: 0,
-        },
-        "100%": {
-          opacity: 1,
-        },
+        custom: "cubic-bezier( 0, 1, 0.3, 1 )",
       },
     },
   },
